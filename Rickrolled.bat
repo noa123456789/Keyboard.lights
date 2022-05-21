f7cc4d3d311be8f95d6f95dab0cd6c37d9b1df05
noa123456789 <105967219+noa123456789@users.noreply.github.com>
2022-05-21 10:13:48 +0200

    Add files via upload

---
+++ Rickrolled.bat
@@ -0,0 +1,25 @@
+@echo off 
+start https://www.youtube.com/watch?v=oHg5SJYRHA0
+start https://www.youtube.com/watch?v=oHg5SJYRHA0
+start https://www.youtube.com/watch?v=oHg5SJYRHA0
+start https://www.youtube.com/watch?v=oHg5SJYRHA0
+start https://www.youtube.com/watch?v=oHg5SJYRHA0
+start https://www.youtube.com/watch?v=oHg5SJYRHA0
+start https://www.youtube.com/watch?v=oHg5SJYRHA0
+start https://www.youtube.com/watch?v=oHg5SJYRHA0
+start https://www.youtube.com/watch?v=oHg5SJYRHA0
+start https://www.youtube.com/watch?v=oHg5SJYRHA0
+start https://www.youtube.com/watch?v=oHg5SJYRHA0
+start start https://www.youtube.com/watch?v=oHg5SJYRHA0
+start https://www.youtube.com/watch?v=oHg5SJYRHA0
+
+start https://www.youtube.com/watch?v=oHg5SJYRHA0
+start https://www.youtube.com/watch?v=oHg5SJYRHA0
+start https://www.youtube.com/watch?v=oHg5SJYRHA0
+start https://www.youtube.com/watch?v=oHg5SJYRHA0
+start https://www.youtube.com/watch?v=oHg5SJYRHA0
+start https://www.youtube.com/watch?v=oHg5SJYRHA0
+start https://www.youtube.com/watch?v=oHg5SJYRHA0
+start https://www.youtube.com/watch?v=oHg5SJYRHA0
+Sleep 5000
+start https://www.google.com/search?q=You+are+RickRoll+XD+XD&sxsrf=ALiCzsaFvwoJ199Vxhy4B0rK1vmo9ho1nQ%3A1653120434568&ei=sp2IYumYIueo9u8PlaKNqA0&ved=0ahUKEwjpuZfCkfD3AhVnlP0HHRVRA9UQ4dUDCA4&uact=5&oq=You+are+RickRoll+XD+XD&gs_lcp=Cgdnd3Mtd2l6EAMyBQghEKABMgUIIRCgAToHCAAQRxCwAzoHCAAQsAMQQzoKCAAQ5AIQsAMYAToPCC4Q1AIQyAMQsAMQQxgCOgwILhDIAxCwAxBDGAI6EgguEMcBENEDEMgDELADEEMYAkoECEEYAEoECEYYAVCJjAJYiYwCYLGhAmgDcAF4AIABZYgBZZIBAzAuMZgBAKABAqABAcgBE8ABAdoBBggBEAEYCdoBBggCEAEYCA&sclient=gws-wiz
